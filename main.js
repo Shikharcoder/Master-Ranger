d1165c1f92f113e92493107b10bf3b5c7fb3ff00
Shikharcoder <92843337+Shikharcoder@users.noreply.github.com>
2022-01-29 09:15:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+var canvas = new fabric.Canvas('myCanvas');
+
+block_y=120;
+block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	fabric.Image.fromURL(get_image, function (Img) {
+		block_image_object = Img;
+        
+		block_image_object.scaleToWidth(block_image_width);
+		block_image_object.scaleToHeight(block_image_height);
+		block_image_object.set({
+			top: block_y,
+			left: block_x
+		});
+		canvas.add(block_image_object);
+	});
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e) {
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+
+	if (keyPressed == '82') {
+		block_x = 600;
+		new_image('redranger.png')
+	}
+	if (keyPressed == '71') {
+		block_x = 850;
+		new_image('greenranger.png')
+	}
+	if (keyPressed == '89') {
+		block_x = 0;
+		new_image('yellowranger.png')
+	}
+	if (keyPressed == '80') {
+		block_x = 1050;
+		new_image('pinkranger.png')
+	}
+	if (keyPressed == '66') {
+		block_x = 125;
+		new_image('blueranger.png')
+	}
+	
+}
